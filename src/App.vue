ce6e15671d26e63f6f2c146de46fc6aa239c7810
jemmy9211 <jemmy9211@gmail.com>
2023-09-04 00:42:34 +0800

    run tradingvue

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import stock from './components/stock.vue'
+</script>
+
+<template>
+  <div id="container">
+    <stock/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/stock.vue
@@ -0,0 +1,46 @@
+<template>
+    <trading-vue :data="this.$data" :width="this.width" :height="this.height"
+    :color-back="colors.colorBack"
+    :color-grid="colors.colorGrid"
+    :color-text="colors.colorText"></trading-vue>
+</template>
+<script>
+
+import TradingVue from 'trading-vue-js'
+
+export default {
+    name: 'app',
+    components: { TradingVue },
+    methods: {
+        onResize(event) {
+            this.width = window.innerWidth
+            this.height = window.innerHeight
+        }
+    },
+    mounted() {
+        window.addEventListener('resize', this.onResize)
+    },
+    beforeDestroy() {
+        window.removeEventListener('resize', this.onResize)
+    },
+    data() {
+        return {
+            colors: {
+                colorBack: '#fff',
+                colorGrid: '#eee',
+                colorText: '#333',
+            },
+            width: window.innerWidth,
+            height: window.innerHeight,
+            ohlcv: [
+                [ 1551128400000, 33,  37.1, 14,  14,  196 ],
+                [ 1551132000000, 13.7, 30, 6.6,  30,  206 ],
+                [ 1551135600000, 29.9, 33, 21.3, 21.8, 74 ],
+                [ 1551139200000, 21.7, 25.9, 18, 24,  140 ],
+                [ 1551142800000, 24.1, 24.1, 24, 24.1, 29 ],
+            ]
+        }
+    }
+}
+
+</script>
